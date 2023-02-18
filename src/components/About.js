b0a299797d7a645879838e908c66f554b163bc6f
emilydta <emilydagata@gmail.com>
2023-02-18 18:15:39 +1100

    add extra slide to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,34 +1,28 @@
-import { default as aboutMe } from '../../images/about_me.svg';
-import '../../stylesheets/About.css';
-import toggleAnimation from './utils/animationFunctions';
+import { default as aboutMe } from '../images/about_me.svg';
+import '../stylesheets/About.css';
 import Intro from './sub-components/Intro';
 import Skills from './sub-components/Skills';
 import { useState } from 'react';
 
 function About() {
-    const [currentSlide, setCurrentSlide] = useState({intro});
-    const [toggleFade, setToggleFade] = useState('');
-
-    const nextSlide = (slide) => {
-        toggleAnimation(setToggleFade);
-        setCurrentSlide(slide);
-    }
+    const [currentSlide, setCurrentSlide] = useState(0);
 
     return (
         <div className='about'>
             <div className='about-slide-bg-container'>
-                <div className='about-slide-bg'>
+                <div className={`about-slide-bg ${currentSlide === 0 && 'slide-0-height'}`}>
                     <div className='about-content'>
-                        {introContinued}
-                        <div></div>
+                        <Intro
+                            currentSlide={currentSlide}
+                            setCurrentSlide={setCurrentSlide}
+                        />
                     </div>
                 </div>
             </div>
-
             <div className='about-image-container'>
                 <img src={aboutMe} alt='About me'></img>
             </div>
-            <Skiils />
+            <Skills />
         </div>
     )
 }
--- src/components/sub-components/Intro.js
+++ src/components/sub-components/Intro.js
@@ -1,12 +1,24 @@
-function Intro() {
-    const SlideOne = <div className='about-info'>
+import { useState } from 'react';
+import toggleAnimation from '../utils/animationFunctions';
+
+function Intro({
+    currentSlide,
+    setCurrentSlide
+}) {
+    const [toggleFade, setToggleFade] = useState('');
+
+    const nextSlide = () => {
+        toggleAnimation(setToggleFade);
+        currentSlide === 0 ? setCurrentSlide(1) : setCurrentSlide(0);
+    }
+    const Slide0 = <div className={`about-info ${toggleFade}`}>
         <h1 className='about-heading'>HELLO!</h1>
         <p className='about-paragraph'>
             I'm a fullstack developer from Melbourne, Australia with a keen interest for tackling problems through code and design.
         </p>
     </div>
 
-    const SlideTwo = <div className='about-info'>
+    const Slide1 = <div className={`about-info extended ${toggleFade}`}>
         <p className='about-paragraph'>
             My interests span a diverse range, but coding, music, and art, are my primary pursuits. Each of these areas allow me to express myself and my creativity.
         </p>
@@ -14,9 +26,19 @@ function Intro() {
             When it comes to coding, I thoroughly enjoy the process of problem-solving and building useful and aesthetically appealing applications.
         </p>
         <p className='about-paragraph'>
-            When I'm not coding, I'm typically preoccupied with creating music or art. Whether I'm making my own tunes or sketching a new character design, it's always a blast. This creative fusion gives me the opportunity to bring my ideas to life in ways that are both functional and visually exciting.
+            When I'm not coding, I'm typically preoccupied with creating music or art. Whether I'm making my own tunes or sketching a new character design, it's always a fun time. This creative fusion gives me the opportunity to bring my ideas to life in ways that are both functional and visually exciting.
         </p>
     </div>
+
+    const slideArray = [Slide0, Slide1];
+    return (
+        <>
+            {slideArray[currentSlide]}
+            <div className='next-buttons'>
+                <p onClick={() => nextSlide()}>{currentSlide === 0 ? '→' : '←'}</p>
+            </div>
+        </>
+    )
 }
 
 export default Intro;
